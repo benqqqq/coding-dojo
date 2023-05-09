3c984da63fe5e79410ad885df171281324d11459
mitchwu <benq7879@gmail.com>
2023-05-09 09:41:38 +0800

    chore: learn and try tailwindcss

--- react-dojo/src/TicTacToe/Board.tsx
+++ react-dojo/src/TicTacToe/Board.tsx
@@ -1,7 +1,19 @@
 export function Board() {
   return (
     <>
-      <h1>Board</h1>
+      <div className="flex h-screen flex-col items-center justify-center">
+        <div className="grid grid-cols-3">
+          <div className="h-24 w-24 border-2 border-black cursor-pointer bg-green-200 rounded"></div>
+          <div className="h-24 w-24 border-2 border-black cursor-pointer bg-green-200 rounded"></div>
+          <div className="h-24 w-24 border-2 border-black cursor-pointer bg-green-200 rounded"></div>
+          <div className="h-24 w-24 border-2 border-black cursor-pointer bg-green-200 rounded"></div>
+          <div className="h-24 w-24 border-2 border-black cursor-pointer bg-green-200 rounded"></div>
+          <div className="h-24 w-24 border-2 border-black cursor-pointer bg-green-200 rounded"></div>
+          <div className="h-24 w-24 border-2 border-black cursor-pointer bg-green-200 rounded"></div>
+          <div className="h-24 w-24 border-2 border-black cursor-pointer bg-green-200 rounded"></div>
+          <div className="h-24 w-24 border-2 border-black cursor-pointer bg-green-200 rounded"></div>
+        </div>
+      </div>
     </>
   );
 }
