a7904b2950777fcd73beb87fcafa6d0b8c669227
mitchwu <benq7879@gmail.com>
2023-05-30 17:33:34 +0800

    feat: add combine js function

---
+++ random/combine.js
@@ -0,0 +1,21 @@
+
+
+const combine = (str) => {
+  if (typeof str === 'undefined') {
+    return '';
+  }
+  // a function that return str
+  return (nestedStr) => {
+    if (typeof nestedStr === 'undefined') {
+      return str;
+    }
+    return combine(`${str}-${nestedStr}`);
+  };
+}
+
+
+
+console.log(combine()); // ''
+console.log(combine('a')()); // a
+console.log(combine('a')('b')()); // a-b
+console.log(combine('a')('b')('c')()); // a-b-c
